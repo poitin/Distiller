6ab25ef0dee78794711a0a9a3f5fc47889f6021d
gsv <rsdpisuy@gmail.com>
2021-08-24 15:48:26 +0300

    More accurate programs comparison. More tests.

--- test/DistillTest.hs
+++ test/DistillTest.hs
@@ -5,6 +5,7 @@ import Test.HUnit
 import Term
 import Trans (dist)
 import System.Timeout
+import Data.List
 
 defaultTimeout = 10 * 1000000
 
@@ -20,13 +21,17 @@ createDistillationTest fileToDistill importsForDistill fileWithGold importsForGo
   distillationResult <- timeout timeoutForDistillation (pure $ dist progToDistill)
   case distillationResult of
     Nothing -> assertFailure $ "Timeout in " ++ show (timeoutForDistillation `div` 1000000) ++ " seconds is expired."
-    Just (distilledProg,x) -> do
-      (expectedProg,x) <- load fileWithGold importsForGold
-      return $ TestCase $ assertEqual ("Distilled program for " ++ fileToDistill ++ " should be equals to " ++ fileWithGold ++ ".") expectedProg distilledProg
+    Just (mainOfDistilledProg, x) -> do
+      (mainOfExpectedProg, y) <- load fileWithGold importsForGold
+      let actual = ("main", ([], mainOfDistilledProg)) : x
+      let expected = ("main", ([], mainOfExpectedProg)) : y
+      return $ TestCase $ assertEqual ("Distilled program for " ++ fileToDistill ++ " should be equals to " ++ fileWithGold ++ ".") expected (expected `intersect` actual) 
 
 
 distillerTests :: IO Test
 distillerTests = do 
   test1 <- createDistillationTest "test1" "examples/" "test1_gold" "examples/" defaultTimeout
+  testAppApp <- createDistillationTest "appapp" "inputs/" "gold/appapp_gold" "inputs/" defaultTimeout
+  testMap <- createDistillationTest "map" "inputs/" "gold/map_gold" "inputs/" defaultTimeout
   
-  return $ TestList [test1]
+  return $ TestList [test1, testAppApp, testMap]
