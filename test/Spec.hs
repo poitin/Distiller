660ee922bf4dd54b75ed360d61e250b56dcc18ed
gsv <rsdpisuy@gmail.com>
2021-08-24 13:46:38 +0300

    An example of test for distiller.

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,6 +8,6 @@ import Test.Framework.Providers.HUnit
 main :: IO ()
 main = do
     evalTests <- EvalTest.eval1Test
-    distillTests <- DistillTest.distill1Test
+    distillTests <- DistillTest.distillerTests
     let distillEvalTests = []
     defaultMainWithOpts (hUnitTestToTests $ TestList $ evalTests : distillTests : distillEvalTests) mempty
