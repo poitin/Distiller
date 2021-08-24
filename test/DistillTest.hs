2385734d3b17e05fa890e1852cad68acb80e2cfb
gsv <rsdpisuy@gmail.com>
2021-08-24 13:46:38 +0300

    An example of test for distiller.

--- test/DistillTest.hs
+++ test/DistillTest.hs
@@ -1,25 +1,32 @@
-module DistillTest (distill1Test) where
+module DistillTest (distillerTests) where
 
 import Helpers
 import Test.HUnit
 import Term
 import Trans (dist)
+import System.Timeout
 
-distill1Test :: IO Test
-distill1Test = let
-  c = "import Bool\n" ++
-      "import QTree\n" ++
-      "import Matrices\n" ++
-      "main = kron (not) (or) (mask m1 msk) (m2)\n" in
-  case parseModule c of
-    Left s -> assertFailure "Oops"
-    Right (fs, d) -> do
-        s <- loadProg fs [] d $ Just "examples/"
-        case s of
-          Nothing -> assertFailure "Ooops"
-          Just (t, d) -> do
-            expectedProg' <- expectedProg
-            return $ TestList [TestCase $ assertEqual "lala" expectedProg' (showProg $ dist (t,d))]
-            
-expectedProg :: IO String
-expectedProg = readFile "examples/MasksDistilled.pot"
+defaultTimeout = 10 * 1000000
+
+load root imports =
+  do s <- loadProg [root] [] [] $ Just imports
+     case s of
+      Nothing -> assertFailure $ "Program loading failed for program " ++ root ++ " and imports " ++ imports ++ "."
+      Just (t, d) -> return (t,d)
+  
+
+createDistillationTest fileToDistill importsForDistill fileWithGold importsForGold timeoutForDistillation =  do
+  progToDistill <- load fileToDistill importsForDistill
+  distillationResult <- timeout timeoutForDistillation (pure $ dist progToDistill)
+  case distillationResult of
+    Nothing -> assertFailure $ "Timeout in " ++ show (timeoutForDistillation `div` 1000000) ++ " seconds is expired."
+    Just (distilledProg,x) -> do
+      (expectedProg,x) <- load fileWithGold importsForGold
+      return $ TestCase $ assertEqual ("Distilled program for " ++ fileToDistill ++ " should be equals to " ++ fileWithGold ++ ".") expectedProg distilledProg
+
+
+distillerTests :: IO Test
+distillerTests = do 
+  test1 <- createDistillationTest "test1" "examples/" "test1_gold" "examples/" defaultTimeout
+  
+  return $ TestList [test1]
