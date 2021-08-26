7ce30053a222a0aa00f1cab671c34798110db3bb
gsv <rsdpisuy@gmail.com>
2021-08-26 11:08:44 +0300

    More tests. Distillation exceptions handling is added.

--- test/Test/DistillTest.hs
+++ test/Test/DistillTest.hs
@@ -5,10 +5,10 @@ import Test.Tasty
 import Test.Tasty.HUnit
 import Term
 import Trans (dist)
-import System.Timeout
 import Data.List
 import Data.Maybe (fromJust, isJust)
 import Text.Printf ( printf )
+import Control.Exception
 
 defaultTimeout :: Integer
 defaultTimeout = 2000000
@@ -24,22 +24,29 @@ createDistillationTest :: String -> String -> String -> String -> Integer -> IO 
 createDistillationTest fileToDistill importsForDistill fileWithGold importsForGold timeoutForDistillation =  do
   progToDistill <- load fileToDistill importsForDistill
   (mainOfExpectedProg, y) <- fromJust <$> load fileWithGold importsForGold -- parsing golden file should always succeed
+  let testCaseName = printf "Distillation of %s" fileToDistill
   if isJust progToDistill
   then do
-    let (mainOfDistilledProg, x) = dist (fromJust progToDistill)
-    let actual = ("main", ([], mainOfDistilledProg)) : x
-    let expected = ("main", ([], mainOfExpectedProg)) : y
-    let assertion = expected `intersect` actual @?= expected
-    let testCaseName = printf "Distillation: %s" fileToDistill
-    return $ timeOutTest timeoutForDistillation testCaseName assertion
+    distillationResult <- try (evaluate $ dist (fromJust progToDistill)) :: IO (Either SomeException (Term, [(String, ([String], Term))]))
+    case distillationResult of
+      Left ex -> do 
+        let assertion = assertFailure $ printf "program: %s; imports: %s; exception: %s" fileToDistill importsForDistill (show ex)
+        return $ testCase testCaseName assertion
+      Right  (mainOfDistilledProg, x) -> do
+        let actual = ("main", ([], mainOfDistilledProg)) : x
+        let expected = ("main", ([], mainOfExpectedProg)) : y
+        let assertion = expected `intersect` actual @?= expected        
+        return $ timeOutTest timeoutForDistillation testCaseName assertion
   else do
     let testCaseName = printf "Parsing: %s" fileToDistill
-    let assertion = isJust progToDistill @? printf "program: %s; imports: %s." fileToDistill importsForDistill
+    let assertion = assertFailure $ printf "program: %s; imports: %s." fileToDistill importsForDistill
     return $ testCase testCaseName assertion
+
+-- Basic tests
 
 test_distillerBasicTest1 :: IO TestTree
 test_distillerBasicTest1 =
-  createDistillationTest "test1" "examples/" "test1_gold" "examples/" defaultTimeout
+  createDistillationTest "plusassoc" "inputs/" "gold/plusassoc_gold" "inputs/" defaultTimeout
 
 test_distillerBasicTest2 :: IO TestTree
 test_distillerBasicTest2 = do
@@ -48,6 +55,17 @@ test_distillerBasicTest2 = do
 test_distillerBasicTest3 :: IO TestTree
 test_distillerBasicTest3 = do
   createDistillationTest "map" "inputs/" "gold/map_gold" "inputs/" defaultTimeout
+
+test_distillerBasicTest4 :: IO TestTree
+test_distillerBasicTest4 =
+  createDistillationTest "pluscomm" "inputs/" "gold/pluscomm_gold" "inputs/" defaultTimeout
+
+test_distillerBasicTest5 :: IO TestTree
+test_distillerBasicTest5 =
+  createDistillationTest "revrev" "inputs/" "gold/revrev_gold" "inputs/" defaultTimeout
+
+
+-- Linear algebra tests
 
 test_distillerLinearAlgebraTest1 :: IO TestTree
 test_distillerLinearAlgebraTest1 = do
